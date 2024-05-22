eb9969dc6df52872415959b8ddd9698937b3f193
emayethossen <samir2021hossen@gmail.com>
2024-05-22 11:12:19 +0600

    interface and model create

---
+++ src/modules/products/product.interface.ts
@@ -0,0 +1,18 @@
+export type TVarients={
+    type: string;
+    value: string;
+}
+
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: [TVarients];
+    inventory: {
+        quantity: number;
+        inStock: boolean;
+    };
+};
+

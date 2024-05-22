768372d9e2683ee582ccff85a97070005b671cd7
emayethossen <samir2021hossen@gmail.com>
2024-05-22 14:36:41 +0600

    GET method applied

---
+++ src/modules/products/product.service.ts
@@ -0,0 +1,17 @@
+import { TProduct } from "./product.interface";
+import { Product } from "./product.model";
+
+const createProduct= async (payLoad:TProduct)=>{
+    const result = await Product.create(payLoad);
+    return result;
+}
+
+const getAllProducts= async ()=>{
+    const result = await Product.find();
+    return result;
+}
+
+export const ProductServices = {
+    createProduct,
+    getAllProducts
+}
